9cd4639a1f75f9a68acd4c832cdfa23a86c4e482
Camerinio <cameronhogg18@gmail.com>
2023-06-14 17:49:03 +0100

    dotenv?

--- payday.py
+++ payday.py
@@ -1,6 +1,12 @@
 import discord
 from discord.ext import commands
 from discord.ui.item import Item
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents()
 intents.message_content = True
