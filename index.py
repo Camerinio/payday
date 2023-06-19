a6f6f6d2859071804bee0a5ba47ea534d47c63ff
Camerinio <cameronhogg18@gmail.com>
2023-06-19 09:00:03 +0100

    2 bots one server

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from payday import bot1
 from triads import bot2
 
 token1 = "MTEwOTk5NzE0MTQ5MDkyOTY4NA.GqH6hU.uR-Tp_Kuz0FpOgE2ZwfsFUFDYZOtzZEE33Wwk4"
-token2 = "YOUR_TOKEN2_HERE"
+token2 = "MTEyMDA5ODAzNzI4MTMzMzM4OA.GOsraR.oNlbITGHy3PZ40pkjaEPY8fYnaFX_9jtjbphqU"
 
 bot2.login(token2)
 bot1.login(token1)
