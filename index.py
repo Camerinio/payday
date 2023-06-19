30ce7dc96c683ce7836be69315e5181a95b0b30a
Camerinio <cameronhogg18@gmail.com>
2023-06-19 08:57:55 +0100

    index

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import discord
 from payday import bot1
 from triads import bot2
 
-token1 = "YOUR_TOKEN1_HERE"
+token1 = "MTEwOTk5NzE0MTQ5MDkyOTY4NA.GqH6hU.uR-Tp_Kuz0FpOgE2ZwfsFUFDYZOtzZEE33Wwk4"
 token2 = "YOUR_TOKEN2_HERE"
 
 bot2.login(token2)
