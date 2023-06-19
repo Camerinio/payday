62459e3b7832e7bc75c71250146d0e67b59c5116
Camerinio <cameronhogg18@gmail.com>
2023-06-19 09:18:30 +0100

    revert

--- payday.py
+++ payday.py
@@ -5,48 +5,48 @@ from discord.ui.item import Item
 intents = discord.Intents()
 intents.message_content = True
 
-bot1 = discord.Bot(command_prefix="/")  # Instantiate the Bot1bot1 class from the discord module
+bot = discord.Bot(command_prefix="/")  # Instantiate the Botbot class from the discord module
 
-@bot1.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {bot1.user.name} ({bot1.user.id})')
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
     print('------')
 
-@bot1.slash_command(description="Sends the bot1's latency.") # this decorator makes a slash command
+@bot.slash_command(description="Sends the bot's latency.") # this decorator makes a slash command
 async def ping(ctx): # a slash command will be created with the name "ping"
-    await ctx.respond(f"Pong! Latency is {bot1.latency}")
+    await ctx.respond(f"Pong! Latency is {bot.latency}")
 
 #Start of clean/dirty
-@bot1.slash_command(description="Add 20% to a value which is good for calculating clean cash to dirty")
+@bot.slash_command(description="Add 20% to a value which is good for calculating clean cash to dirty")
 async def dirty(ctx, value: float):
     new_value = value * 1.2
     await ctx.respond(f"The new value is {new_value:.2f}")
 
-@bot1.slash_command(description="Remove 20% to a value which is good for calculating dirty to clean cash")
+@bot.slash_command(description="Remove 20% to a value which is good for calculating dirty to clean cash")
 async def clean(ctx, value: float):
     new_value = value / 1.2
     await ctx.respond(f"The new value is {new_value:.2f}")
 
 # Command: Prices
-@bot1.slash_command(description="What Payday buys bank equipment for")
+@bot.slash_command(description="What Payday buys bank equipment for")
 async def prices(ctx):
     response = "**Thermal:** 40-60k\n**Thermite:** 15-20k\n**Level 3 Access Card:** 25-35k\n**Level 6 Access Card:** 40-60k\n"
     await ctx.respond(response)
 
 #Maths
-@bot1.slash_command(description="adds two values")
+@bot.slash_command(description="adds two values")
 async def sum(ctx, a: int, b: int):
   await ctx.respond(a + b)
 
-@bot1.slash_command(description="multiplys two values")
+@bot.slash_command(description="multiplys two values")
 async def multiply(ctx, a: int, b: int):
   await ctx.respond(a * b)
 
-@bot1.slash_command(description="minuses two values")
+@bot.slash_command(description="minuses two values")
 async def minus(ctx, a: int, b: int):
   await ctx.respond(a - b)
   
-@bot1.slash_command(description="divides two values")
+@bot.slash_command(description="divides two values")
 async def divide(ctx, a: int, b: int):
   await ctx.respond(a / b)
 
@@ -68,7 +68,7 @@ class MyView(discord.ui.View):
     async def third_button_callback(self, button, interaction):
         await interaction.response.send_message("Join Radio 766")
     
-    @bot1.slash_command(description="Important Payday information") # Create a slash command
+    @bot.slash_command(description="Important Payday information") # Create a slash command
     @commands.has_role("Gang member")
     async def important(ctx):
         await ctx.respond("Important information", view=MyView()) # Send a message with our View class that contains the button
@@ -93,19 +93,19 @@ class MyView1(discord.ui.View):
         await member.add_roles(role)
         await interaction.response.send_message("You have now received the Contractor role.", ephemeral=True)
 
-@bot1.event
+@bot.event
 async def on_ready():
     # Get the desired channel where you want to send the message
-    channel = bot1.get_channel(1102730487379808326)
+    channel = bot.get_channel(1102730487379808326)
     # Send the message with your view class
     view = MyView1()
     message = await channel.send("React to receive your role.", view=view)
 
-@bot1.slash_command(descirption="Displays the role request command")
+@bot.slash_command(descirption="Displays the role request command")
 async def roles(ctx):
     await ctx.respond("Role request", view=MyView1())
     
-@bot1.slash_command(pass_context=True, descirption="Purge an amount of messages")
+@bot.slash_command(pass_context=True, descirption="Purge an amount of messages")
 @commands.has_role("Controller")
 async def purge(ctx, limit: int):
     try:
@@ -116,7 +116,7 @@ async def purge(ctx, limit: int):
     except Exception as e:
         await ctx.send(f"An error occurred: {e}")
 
-@bot1.slash_command(descirption="The commands that we have")
+@bot.slash_command(descirption="The commands that we have")
 async def help(ctx):
     embed = discord.Embed(
         
@@ -126,16 +126,18 @@ async def help(ctx):
         
     )
     embed.add_field(name="/dirty", value="Add 20% to a value", inline=True)
-    embed.add_field(name="/ping", value="Sends the bot1's latency", inline=True)
+    embed.add_field(name="/ping", value="Sends the bot's latency", inline=True)
     embed.add_field(name="/prices", value="Bank equipment prices", inline=True)
     embed.add_field(name="/multiply", value="Multiplys two values", inline=True)
     embed.add_field(name="/add", value="Adds two values", inline=True)
     embed.add_field(name="/divide", value="Divides two values", inline=True)
     embed.add_field(name="/minus", value="Minuses two values", inline=True)
  
-    embed.set_footer(text="Bot1bot1 created by Camerinio.") # footers can have icons too
+    embed.set_footer(text="Botbot created by Camerinio.") # footers can have icons too
     embed.set_author(name="Payday", icon_url="")
     embed.set_thumbnail(url="https://i.imgur.com/6F4vy46.png")
     embed.set_image(url="https://i.imgur.com/vvMxzUL.png")
  
     await ctx.respond(embed=embed) # Send the embed with some text
+
+bot.run("MTEwOTk5NzE0MTQ5MDkyOTY4NA.GqH6hU.uR-Tp_Kuz0FpOgE2ZwfsFUFDYZOtzZEE33Wwk4")
--- triads.py
+++ triads.py
@@ -5,43 +5,43 @@ from discord.ui.item import Item
 intents = discord.Intents()
 intents.message_content = True
 
-bot2 = discord.Bot(command_prefix="/")  # Instantiate the Bot2bot2 class from the discord module
+bot = discord.Bot(command_prefix="/")  # Instantiate the Bot class from the discord module
 
-@bot2.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {bot2.user.name} ({bot2.user.id})')
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
     print('------')
 
 #start of clean/dirty
-@bot2.slash_command(description="Add 20% to a value which is good for calculating clean cash to dirty")
+@bot.slash_command(description="Add 20% to a value which is good for calculating clean cash to dirty")
 async def dirty(ctx, value: float):
     new_value = value * 1.2
     await ctx.respond(f"The new value is {new_value:.2f}")
 
-@bot2.slash_command(description="Remove 20% to a value which is good for calculating dirty to clean cash")
+@bot.slash_command(description="Remove 20% to a value which is good for calculating dirty to clean cash")
 async def clean(ctx, value: float):
     new_value = value / 1.2
     await ctx.respond(f"The new value is {new_value:.2f}")
 
 #maths
-@bot2.slash_command(description="adds two values")
+@bot.slash_command(description="adds two values")
 async def sum(ctx, a: int, b: int):
   await ctx.respond(a + b)
 
-@bot2.slash_command(description="multiplys two values")
+@bot.slash_command(description="multiplys two values")
 async def multiply(ctx, a: int, b: int):
   await ctx.respond(a * b)
 
-@bot2.slash_command(description="minuses two values")
+@bot.slash_command(description="minuses two values")
 async def minus(ctx, a: int, b: int):
   await ctx.respond(a - b)
   
-@bot2.slash_command(description="divides two values")
+@bot.slash_command(description="divides two values")
 async def divide(ctx, a: int, b: int):
   await ctx.respond(a / b)
 
 #purge
-@bot2.slash_command(pass_context=True, descirption="Purge an amount of messages")
+@bot.slash_command(pass_context=True, descirption="Purge an amount of messages")
 @commands.has_role("The Family")
 async def purge(ctx, limit: int):
     try:
@@ -52,7 +52,7 @@ async def purge(ctx, limit: int):
     except Exception as e:
         await ctx.send(f"An error occurred: {e}")
 
-@bot2.slash_command(descirption="The commands that we have")
+@bot.slash_command(descirption="The commands that we have")
 async def help(ctx):
     embed = discord.Embed(
         
@@ -62,16 +62,19 @@ async def help(ctx):
         
     )
     embed.add_field(name="/dirty", value="Add '20%' to a value", inline=True)
-    embed.add_field(name="/ping", value="Sends the bot2's latency", inline=True)
+    embed.add_field(name="/ping", value="Sends the bot's latency", inline=True)
     embed.add_field(name="/clean", value="Takes away '20%' from a value" , inline=True)
     embed.add_field(name="/multiply", value="Multiplys two values", inline=True)
     embed.add_field(name="/add", value="Adds two values", inline=True)
     embed.add_field(name="/divide", value="Divides two values", inline=True)
     embed.add_field(name="/minus", value="Minuses two values", inline=True)
  
-    embed.set_footer(text="Bot2bot2 created by Camerinio.") # footers can have icons too
+    embed.set_footer(text="Bot created by Camerinio.") # footers can have icons too
     embed.set_author(name="Payday", icon_url="")
     embed.set_thumbnail(url="https://i.imgur.com/6zMSFoM.png")
     embed.set_image(url="https://i.imgur.com/6zMSFoM.png")
  
     await ctx.respond(embed=embed) # Send the embed with some text")
+
+    
+bot.run("MTEyMDA5ODAzNzI4MTMzMzM4OA.GOsraR.oNlbITGHy3PZ40pkjaEPY8fYnaFX_9jtjbphqU")
